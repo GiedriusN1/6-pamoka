ed1aa332dc2a502e30d0656b2858b03eb03cb124
GiedriusNekrasas <giedrius.nekrasas@gmail.com>
2020-02-19 10:30:22 +0200

    Update SachmatuLenta.java

--- SachmatuLenta.java
+++ SachmatuLenta.java
@@ -5,15 +5,26 @@ public class SachmatuLenta
 
 	public static void main(String[] args) 
 	{
-		BigInteger vienas, du, grudu;
+		BigInteger vienas, du, next, grudu;
  	    int langeliu =  64;
 	    du = new BigInteger("2");
 		vienas = new BigInteger("1");
-        grudu =  du.pow(langeliu);
-        BigInteger rez = grudu.subtract(vienas); 
+		next  = new BigInteger("1"); // keliant laipsniu, nereikia siu eiluciu
+		grudu = new BigInteger("1"); //
+		
+       // grudu =  du.pow(langeliu);
+       // grudu = grudu.subtract(vienas); 
+	   
+	   for (int i = 1; i < langeliu; i++) {
+		   next = next.multiply(du);
+		   grudu = grudu.add(next);
+	   }
 	
-	  System.out.print(rez);	
+	  System.out.printf("\n" + "Sachmatu lentoje bus " + grudu + " grudu");	
 		
  
   }
  }
+
+//18446744073709551615
+//18446744073709551615
